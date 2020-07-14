9998e083cefb03b6b4eea9cf05a66662d2a998b2
Iury Vasconcelos <iury0393@gmail.com>
2020-07-14 08:20:10 -0300

    Realizando o upload no disco

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer')
 
-routes.get('/', (req, res) => {
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
+  console.log(req.file);
+
   return res.json({
-    
+
   });
 });
 
